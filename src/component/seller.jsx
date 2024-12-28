1ff439c7cbf9d12529282367ec0e8627135e3410
Anie22 <francisaniebiet678@gmail.com>
2024-12-28 18:42:18 +0100

    updates

--- src/component/seller.jsx
+++ src/component/seller.jsx
@@ -16,7 +16,7 @@ export const Seller = () => {
                     <h3 className="text-xl lg:text-3xl uppercase font-bold">Our best seller</h3>
                     <p className="font-medium text-lg">Fall in love with every piece</p>
                 </div>
-                <div className="flex items-center justify-center flex-col lg:flex-row w-full gap-3">
+                <div className="flex items-center justify-start flex-col lg:flex-row w-full gap-3">
                     {popularSales.map((sale, i) => (
                         <div className="w-full lg:w-2/5 h-fit flex-1 rounded-lg shadow-md shadow-slate-400 overflow-hidden" key={i}>
                             <div className="flex items-start justify-start flex-col gap-3 w-full h-fit">
