4e2fb40983a7475b1b81f77bb1dc7cdbfa3043e4
Anie22 <francisaniebiet678@gmail.com>
2025-05-18 07:33:09 +0100

    updates: added login authetication

--- src/accounts/login.jsx
+++ src/accounts/login.jsx
@@ -1,31 +1,121 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { FormCard } from "../component/form";
 import login from "../assets/login.jpg";
+import { postToApi } from "../service/baseAPI";
+import { Model } from "../component/modal/Modal";
 
 export const LoginAccount = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState({});
+  const [loader, setLoader] = useState(false);
+  const [modalMsg, setModalMsg] = useState({
+    modal: true,
+    message: "",
+    icon: "",
+    display: true,
+    direction: "",
+  });
+
   const form = [
     { for: "email", label: "email", type: "email", name: "email" },
     { for: "password", label: "password", type: "password", name: "password" },
   ];
 
+  useEffect(() => {
+    
+    setModalMsg({
+      modal: false,
+      message: "login successfully",
+      direction: "/",
+      display: false,
+      icon: "success",
+    });
+  }, [])
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    setError({ ...error, [name]: undefined });
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const symbols = /[!#$%^&*()]/;
+    const newErr = {};
+
+    if (!formData.email) {
+      newErr.email = "email required";
+    } else if (symbols.test(formData.email)) {
+      newErr.email =
+        "email must not contain or starts with any of this symbols";
+    } else if (!formData.password) {
+      newErr.password = "password required";
+    } else {
+      const url = "login";
+      setLoader(true);
+      try {
+        const response = await postToApi(url, formData);
+        if (response) {
+          sessionStorage.setItem("MVtoken", response.access_token);
+          localStorage.setItem(
+            "userData",
+            JSON.stringify({
+              name: `${response.user_name}`,
+              pic: `${response.profile_pic}`,
+            })
+          );
+        }
+      } catch (err) {
+        console.log("err", err);
+        return;
+      } finally {
+        setLoader(false);
+      }
+    }
+
+    if (Object.keys(newErr).length > 0) {
+      setError(newErr);
+      return;
+    }
+  };
+
   return (
-    <FormCard
-      authImg={login}
-      heading="ViksGallery"
-      authMessage="Welcome back"
-      formInHolder={form}
-      fgTxt="forgot password?"
-      btnText="Login"
-      singleSubLink={true}
-      link="/signUp"
-      subLink="Sign up now"
-      formStyle="flex lg:h-screen h-svh w-full relative"
-      imageHldStyle="lg:h-screen h-svh lg:w-2/5 w-full overflow-hidden"
-      subImgHoldStyle="lg:h-screen h-svh w-full"
-      headingStyle="font-semibold xl:text-4xl lg:text-3xl text-2xl lg:text-auth text-white"
-      formHoldStyle="lg:w-3/5 w-full py-6 lg:px-8 md:px-6 px-4 flex flex-col gap-9 lg:relative absolute bg-ab-bg-color lg:bg-transparent h-full"
-      innerFormStyle="lg:w-4/5 flex flex-col gap-5 w-full bg-white lg:bg-transparent p-3 rounded-lg"
-      authMsgStyle="text-center font-semibold xl:text-3xl text-xl text-auth capitalize"
-    />
+    <>
+      <FormCard
+        authImg={login}
+        heading="ViksGallery"
+        authMessage="Welcome back"
+        formInHolder={form.map((f) => ({
+          ...f,
+          value: formData[f.name],
+        }))}
+        error={error}
+        handleChange={(e) => handleInput(e)}
+        handleSubmit={(e) => handleFormSubmit(e)}
+        loading={loader}
+        fgTxt="forgot password?"
+        btnText="Login"
+        singleSubLink={true}
+        link="/signUp"
+        subLink="Sign up now"
+        formStyle="flex lg:h-screen h-svh w-full relative"
+        imageHldStyle="lg:h-screen h-svh lg:w-2/5 w-full overflow-hidden"
+        subImgHoldStyle="lg:h-screen h-svh w-full"
+        headingStyle="font-semibold xl:text-4xl lg:text-3xl text-2xl lg:text-auth text-white"
+        formHoldStyle="lg:w-3/5 w-full py-6 lg:px-8 md:px-6 px-4 flex flex-col gap-9 lg:relative absolute bg-ab-bg-color lg:bg-transparent h-full"
+        innerFormStyle="lg:w-4/5 flex flex-col gap-5 w-full bg-white lg:bg-transparent p-3 rounded-lg"
+        authMsgStyle="text-center font-semibold xl:text-3xl text-xl text-auth capitalize"
+      />
+      <Model
+        modal={modalMsg.modal}
+        modalDisplay={modalMsg.display}
+        icon={modalMsg.icon}
+        message={modalMsg.message}
+        direction={modalMsg.direction}
+      />
+    </>
   );
 };
--- src/component/form.jsx
+++ src/component/form.jsx
@@ -1,98 +1,139 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-
-export const FormCard = ({ authImg, formStyle, imageHldStyle, subImgHoldStyle, formHoldStyle, headingStyle, heading, innerFormStyle, authMessage, subAuthMessage, authStyle, authMsgStyle, formInHolder, error, fgTxt, btnText, link, subLink, singleSubLink, SubLink}) => {
-    return (
-      <div className="w-full flex items-start">
-        <div className="w-full relative">
-          <div className={`${formStyle}`}>
-            {authImg && (
-              <div className={`${imageHldStyle}`}>
-                <div className={`${subImgHoldStyle}`}>
-                  <img className="h-full w-full" src={authImg} alt="" />
+export const FormCard = ({
+  authImg,
+  formStyle,
+  imageHldStyle,
+  subImgHoldStyle,
+  formHoldStyle,
+  headingStyle,
+  heading,
+  innerFormStyle,
+  authMessage,
+  subAuthMessage,
+  authStyle,
+  authMsgStyle,
+  formInHolder,
+  error,
+  fgTxt,
+  btnText,
+  link,
+  subLink,
+  singleSubLink,
+  SubLink,
+  handleChange,
+  handleSubmit,
+  loading,
+}) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    if (btnText === "Send reset link") {
+      navigate("/update-password");
+    }
+  };
+  return (
+    <div className="w-full flex items-start">
+      <div className="w-full relative">
+        <div className={`${formStyle}`}>
+          {authImg && (
+            <div className={`${imageHldStyle}`}>
+              <div className={`${subImgHoldStyle}`}>
+                <img className="h-full w-full" src={authImg} alt="" />
+              </div>
+            </div>
+          )}
+          <div className={`${formHoldStyle}`}>
+            <div className={`${headingStyle}`}>
+              <h2>{heading}</h2>
+            </div>
+            <div className="w-full flex flex-col items-center justify-start">
+              <div className={`${innerFormStyle}`}>
+                <div className={`${authStyle}`}>
+                  <h5 className={`${authMsgStyle}`}>{authMessage}</h5>
+                  {subAuthMessage && <p>{subAuthMessage}</p>}
                 </div>
-              </div>
-            )}
-            <div className={`${formHoldStyle}`}>
-              <div className={`${headingStyle}`}>
-                <h2>{heading}</h2>
-              </div>
-              <div className="w-full flex flex-col items-center justify-start">
-                <div className={`${innerFormStyle}`}>
-                  <div className={`${authStyle}`}>
-                    <h5 className={`${authMsgStyle}`}>{authMessage}</h5>
-                    {subAuthMessage && <p>{subAuthMessage}</p>}
-                  </div>
-                  <div>
-                    <form action="">
-                      <div className="flex flex-col gap-3">
-                        <div className="flex flex-col gap-5 items-start w-full">
-                          {formInHolder.map((fih, i) => (
-                            <div
-                              className="flex flex-col items-start gap-0 w-full capitalize"
-                              key={i}
+                <div>
+                  <form method="post" onSubmit={handleSubmit}>
+                    <div className="flex flex-col gap-3">
+                      <div className="flex flex-col gap-5 items-start w-full">
+                        {formInHolder.map((fih, i) => (
+                          <div
+                            className="flex flex-col items-start gap-0 w-full capitalize"
+                            key={i}
+                          >
+                            <label
+                              className="text-label font-normal xl:text-lg text-base"
+                              htmlFor={fih.for}
                             >
-                              <label
-                                className="text-label font-normal xl:text-lg text-base"
-                                htmlFor={fih.for}
-                              >
-                                {fih.label}
-                              </label>
-                              <div className="w-full flex flex-col gap-1">
-                                <input
-                                  className="border-b border-bdr w-full h-10 outline-none py-1 px-2 bg-transparent"
-                                  type={fih.type}
-                                  name={fih.name}
-                                />
-                                <p className="text-sm text-danger capitalize font-normal">
-                                  {error}
-                                </p>
-                              </div>
+                              {fih.label}
+                            </label>
+                            <div className="w-full flex flex-col gap-1">
+                              <input
+                                className="border-b border-bdr w-full h-10 outline-none py-1 px-2 bg-transparent"
+                                type={fih.type}
+                                name={fih.name}
+                                value={fih.value}
+                                onChange={handleChange}
+                              />
+                              <p className="text-sm text-danger capitalize font-normal">
+                                {error[fih.name]}
+                              </p>
                             </div>
+                          </div>
+                        ))}
+                      </div>
+                      {fgTxt && (
+                        <div className="md:text-base text-sm font-normal capitalize">
+                          <Link to="/reset-pass">{fgTxt}</Link>
+                        </div>
+                      )}
+                      {btnText && (
+                        <div className="w-full text-center capitalize text-lg text-white">
+                          <button
+                            className="bg-auth h-fit w-full rounded-md flex item-center justify-center py-3"
+                            type="submit"
+                          >
+                            {loading ? (
+                              <span className="border-white border-t-transparent border-b-solid border-[3px] rounded-full h-7 w-7 animate-spin flex"></span>
+                            ) : (
+                              <span>{btnText}</span>
+                            )}
+                          </button>
+                        </div>
+                      )}
+                      {singleSubLink ? (
+                        <div className="md:text-base text-sm font-[600] text-center text-blue-900">
+                          <Link
+                            to={link}
+                            className="flex gap-1 items-center w-full justify-center"
+                          >
+                            <span className="text-gray-800 text-sm">
+                              {subLink === "Sign up now" &&
+                                'Don"t have an account'}
+                              {subLink === "Login" &&
+                                "Already have and account"}
+                            </span>
+                            {subLink}
+                          </Link>
+                        </div>
+                      ) : (
+                        <div className="md:text-base text-sm font-normal capitalize text-center flex justify-between">
+                          {SubLink.map((lnk, i) => (
+                            <Link to={lnk.link} key={i}>
+                              {lnk.subLink}
+                            </Link>
                           ))}
                         </div>
-                        {fgTxt && (
-                          <div className="md:text-base text-sm font-normal capitalize">
-                            <Link to="/reset-pass">{fgTxt}</Link>
-                          </div>
-                        )}
-                        {btnText && (
-                          <div className="w-full text-center capitalize text-lg text-white">
-                            <button className="bg-auth h-12 w-full rounded-md">
-                              {btnText}
-                            </button>
-                          </div>
-                        )}
-                        {singleSubLink ? (
-                          <div className="md:text-base text-sm font-[600] text-center text-blue-900">
-                            <Link to={link}>
-                              {" "}
-                              <span className="text-gray-800 text-sm">
-                                {subLink === "Sign up now"
-                                  ? 'Don"t have an account '
-                                  : "Already have and account "}
-                              </span>
-                              {subLink}
-                            </Link>
-                          </div>
-                        ) : (
-                          <div className="md:text-base text-sm font-normal capitalize text-center flex justify-between">
-                            {SubLink.map((lnk, i) => (
-                              <Link to={lnk.link} key={i}>
-                                {lnk.subLink}
-                              </Link>
-                            ))}
-                          </div>
-                        )}
-                      </div>
-                    </form>
-                  </div>
+                      )}
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
