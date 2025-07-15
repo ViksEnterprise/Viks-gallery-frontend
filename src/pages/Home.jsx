9bd4911fb48fc2ec01e09873a14f25b18cc5c4cd
Aniebiet Francis <102663124+Anie22@users.noreply.github.com>
2025-07-15 12:49:44 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -96,7 +96,7 @@ export const Home = () => {
           {welcomeMSG.map((msg, index) => (
             <div
               key={index}
-              className="text-7xl stroke-text text-transparent flex-initial w-125 font-blod uppercase"
+              className="text-7xl stroke-text text-transparent flex-initial w-5xl font-blod uppercase"
             >
               <h3>{msg.msg}</h3>
             </div>
